55ec8b71f9655e4952549b8c4d294e78927bf4b7
Abdullah <abdullah.ak7@gmail.com>
2017-12-14 16:23:41 +0530

    shell files

---
+++ x86-cmds.sh
@@ -0,0 +1,5 @@
+export cp=~/Desktop/russian-cards-svn
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$cp/z3/z3-4.5.0-x86-ubuntu-14.04/bin
+export PYTHONPATH=$PYTHONPATH:$cp/z3/z3-4.5.0-x86-ubuntu-14.04/bin/python
+export PYTHONPATH=$PYTHONPATH:$cp/v0.1.3/src/cp
+export PYTHONPATH=$PYTHONPATH::$cp/v0.1.3/tests
